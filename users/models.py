8f58969ec205bd49288660c9e52daf0a3902647f
Stanislav <nstanislass@gmail.com>
2023-09-25 17:42:15 +0300

    add model CustomUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    LANGUAGES = (
+        ("UA", "Ukrainian"),
+        ("EN", "English"),
+        ("RU", "Russian"),
+
+    )
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=25, unique=True)
+    native_language = models.CharField(max_length=2, choices=LANGUAGES, default="UA")
+    language_to_learn = models.CharField(max_length=2, choices=LANGUAGES, default='EN')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
