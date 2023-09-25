cf3c8ea18409b391d4dca00ab5625f5e89a5ca77
Stanislav <nstanislass@gmail.com>
2023-09-25 18:39:45 +0300

    add view SignUpView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("homepage")
+    template_name = "users/signup.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SignUpView, self).get_context_data(**kwargs)
+        context["title"] = "Sign UP"
+
+        return context
