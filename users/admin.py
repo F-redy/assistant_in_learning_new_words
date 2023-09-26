e956136199a7db8d4dc7b5f1ddeb00d7c0793c3b
Stanislav <nstanislass@gmail.com>
2023-09-26 19:02:18 +0300

    add model CustomUserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    list_display = ["username", "email", "phone", "native_language", "language_to_learn"]
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'phone', 'native_language', 'language_to_learn')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'password1', 'password2', "email", "phone", "native_language",
+                "language_to_learn"),
+        }),
+    )
