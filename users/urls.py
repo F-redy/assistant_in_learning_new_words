ce9a297d858ae2c5513b3ceead7da607a112c899
Stanislav <nstanislass@gmail.com>
2023-09-27 17:19:34 +0300

    add urls login, logout

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, LogInView, logout_and_redirect_to_login
 
 app_name = 'users'
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name='signup'),
+    path("login/", LogInView.as_view(), name='login'),
+    path("logout/", logout_and_redirect_to_login, name='logout'),
 ]
