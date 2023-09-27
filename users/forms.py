ca4dff59622a0666d51178d5e0b2ef2cc3f4ebe4
Stanislav <nstanislass@gmail.com>
2023-09-27 17:18:34 +0300

    add CustomAuthenticationForm

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,12 @@
 from django.forms import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 
 from users.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """Класс-форма для регистрации пользователей, на основе базового класса регистрации от django"""
+
     class Meta:
         model = CustomUser
         # Поля, которые будут отображены
@@ -16,3 +18,9 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
         # Поля, которые будут отображены
         fields = ("username", "email", "phone", "native_language", "language_to_learn")
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username", "password")
