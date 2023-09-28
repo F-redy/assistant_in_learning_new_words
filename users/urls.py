7545582e97feba4ebaa48a37feaa28b163e5c837
Stanislav <nstanislass@gmail.com>
2023-09-28 16:05:33 +0300

    add profile path

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
-from .views import SignUpView, LogInView, logout_and_redirect_to_login
+from .views import SignUpView, LogInView, UserUpdateView, logout_and_redirect_to_login
 
 app_name = 'users'
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name='signup'),
     path("login/", LogInView.as_view(), name='login'),
+    path("profile/<int:pk>", login_required(UserUpdateView.as_view()), name='profile'),
     path("logout/", logout_and_redirect_to_login, name='logout'),
 ]
