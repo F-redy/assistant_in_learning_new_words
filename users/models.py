2c44485c90d85b0b33ee19b43a5db1dcfdf9ef5e
Stanislav <nstanislass@gmail.com>
2023-10-02 01:13:15 +0300

    delete native_language language_to_learn

--- users/models.py
+++ users/models.py
@@ -3,16 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    LANGUAGES = (
-        ("UA", "Ukrainian"),
-        ("EN", "English"),
-        ("RU", "Russian"),
-
-    )
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=25, unique=True)
-    native_language = models.CharField(max_length=2, choices=LANGUAGES, default="UA")
-    language_to_learn = models.CharField(max_length=2, choices=LANGUAGES, default='EN')
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-    is_teacher = models.BooleanField(default=False)
