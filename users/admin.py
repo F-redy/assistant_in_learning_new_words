282430983b6576c8b9cf80ae5bb1d27edaf88c1c
Stanislav <nstanislass@gmail.com>
2023-10-02 01:12:03 +0300

    delete native_language language_to_learn

--- users/admin.py
+++ users/admin.py
@@ -11,18 +11,16 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
 
-    list_display = ["username", "email", "phone", "native_language", "language_to_learn"]
+    list_display = ["username", "email", "phone"]
 
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'password', 'phone', 'native_language', 'language_to_learn')}),
+        (None, {'fields': ('username', 'email', 'password', 'phone')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': (
-                'username', 'email', 'password1', 'password2', "email", "phone", "native_language",
-                "language_to_learn"),
+            'fields': ('username', 'email', 'password1', 'password2', "email", "phone"),
         }),
     )
