990c558393a594d8c3cea23702a57d8bfb9c1a0d
Stanislav <nstanislass@gmail.com>
2023-10-02 14:14:59 +0300

    change view

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,31 @@
 from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
                                        UserCreationForm)
-from django.forms import forms
+from django import forms
 
 from users.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
     """Класс-форма для регистрации пользователей, на основе базового класса регистрации от django"""
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'placeholder': 'Enter username'}))
+    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Enter email'}))
+    phone = forms.CharField(label='Phone', widget=forms.TextInput(attrs={'placeholder': 'Enter phone'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
+    password2 = forms.CharField(label='Password repeat',
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Enter password again'}))
 
     class Meta:
         model = CustomUser
         # Поля, которые будут отображены
-        fields = ("username", "email", "phone")
+        fields = ('username', 'email', 'phone', 'password1', 'password2')
 
 
 class CustomUserChangeForm(UserChangeForm):
+    username = forms.CharField(label='Changing Username',
+                               widget=forms.TextInput(attrs={'placeholder': 'Enter username'}))
+    email = forms.CharField(label='Changing Email', widget=forms.EmailInput(attrs={'placeholder': 'Enter email'}))
+    phone = forms.CharField(label='Changing Phone', widget=forms.TextInput(attrs={'placeholder': 'Enter phone'}))
+
     class Meta:
         model = CustomUser
         # Поля, которые будут отображены
@@ -22,6 +33,9 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'placeholder': 'Enter username'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
+
     class Meta:
         model = CustomUser
         fields = ("username", "password")
