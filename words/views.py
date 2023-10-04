c9a868cf80698220ed97f24fb44160299a5af5fe
Stanislav <nstanislass@gmail.com>
2023-10-04 19:00:18 +0300

    add RepeatWordsView show_error_words reset

--- words/views.py
+++ words/views.py
@@ -1,12 +1,16 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Count
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView
-from django.db.models import Count
 
 from common.views import DataMixin
 
-from .forms import AddDictionaryForm, AddPairWordForm, ImportWordsForm
+from .forms import AddDictionaryForm, AddPairWordForm, ImportWordsForm, RepeatWordForm
 from .models import Dictionary, PairWord
+from .utils import get_sep, get_existing_originals, get_unique_pairs
 
 
 class AddDictionaryView(DataMixin, CreateView):
@@ -32,7 +36,7 @@ class AddPairWordView(DataMixin, CreateView):
 
     def form_valid(self, form):
         dictionary_id = form.data.get('dictionary')
-        dictionary = Dictionary.objects.get(id=dictionary_id).select_related('user')
+        dictionary = Dictionary.objects.get(id=dictionary_id)
 
         # Добавление слов
         original_words = form.data.getlist('original_word')
@@ -54,29 +58,43 @@ class ImportWordsView(DataMixin, CreateView):
 
     def form_valid(self, form):
         id_dictionary = form.data.get('dictionary')
-        dictionary = Dictionary.objects.get(id=id_dictionary).select_related('user')
+        dictionary = Dictionary.objects.get(id=id_dictionary)
 
         text = form.cleaned_data.get('text')
 
-        if not form.cleaned_data.get('custom_sep'):
-            sep = form.cleaned_data.get('sep_choice')
-        else:
-            sep = form.cleaned_data.get('custom_sep')
-
-        words = []
-
         if text:
-            for pair in text.split('\n'):
-                try:
-                    original, translation = pair.split(sep)
-                    words.append(PairWord(original=original, translation=translation, dictionary=dictionary))
-                except ValueError:
-                    words.append(PairWord(original=pair, translation='', dictionary=dictionary))
+            sep = get_sep(form)
+            db_words = PairWord.objects.filter(dictionary_id=id_dictionary)
+            existing_originals = get_existing_originals(db_words)
+            words_for_db = get_unique_pairs(text, existing_originals, sep, id_dictionary, PairWord)
 
-        if words:
-            PairWord.objects.bulk_create(words)
+            if words_for_db:
+                PairWord.objects.bulk_create(words_for_db)
 
         return HttpResponseRedirect(reverse('words:show_dictionary', kwargs={'dict_slug': dictionary.slug}))
+        # if not form.cleaned_data.get('custom_sep'):
+        #     sep = form.cleaned_data.get('sep_choice')
+        # else:
+        #     sep = form.cleaned_data.get('custom_sep')
+
+        # words = []
+
+        # if text:
+        #     db_words = PairWord.objects.filter(dictionary_id=id_dictionary)
+        #     existing_originals = set(db_word.original for db_word in db_words)
+
+        # for pair in text.split('\n'):
+        #     try:
+        #         original, translation = pair.split(sep)
+        #         if original not in existing_originals:
+        #             words.append(PairWord(original=original, translation=translation, dictionary=dictionary))
+        #     except ValueError:
+        #         words.append(PairWord(original=pair, translation='', dictionary=dictionary))
+
+        # if words:
+        #     PairWord.objects.bulk_create(words)
+        #
+        # return HttpResponseRedirect(reverse('words:show_dictionary', kwargs={'dict_slug': dictionary.slug}))
 
 
 class ShowAllDictionaryUserView(DataMixin, ListView):
@@ -85,10 +103,8 @@ class ShowAllDictionaryUserView(DataMixin, ListView):
     title = 'All Your Dictionaries'
 
     def get_queryset(self):
-        # Получаем все словари пользователя
         dictionaries = Dictionary.objects.filter(user_id=self.request.user.pk)
 
-        # Аннотируем каждый словарь количеством слов в нем
         dictionaries = dictionaries.annotate(count_words=Count('pairword'))
         return dictionaries
 
@@ -105,3 +121,66 @@ class ShowDictionaryView(DetailView):
         context['title'] = self.object.title
         context['word_list'] = PairWord.objects.filter(dictionary=self.object).select_related('dictionary')
         return context
+
+
+class RepeatWordsView(SuccessMessageMixin, DetailView):
+    model = Dictionary
+    template_name = 'words/repeat_words.html'
+    slug_url_kwarg = 'dict_slug'
+
+    def _process_word(self, request, **kwargs):
+        words = PairWord.objects.filter(dictionary__slug=kwargs['dict_slug']).order_by('id')
+        current_word_index = request.session.get('current_word_index', 0)
+
+        if current_word_index < len(words):
+            original = words[current_word_index].original
+            translation = words[current_word_index].translation
+
+            request.session['original'] = original
+            request.session['translation'] = translation
+
+            title = ' '.join(self.kwargs["dict_slug"].split('-')).title()
+            form = RepeatWordForm()
+            context = {'translation': translation, 'title': f'Repeat {title}', 'form': form}
+
+            return render(request, 'words/repeat_words.html', context)
+        else:
+            # Все слова уже отгаданы или список слов пуст
+            request.session['current_word_index'] = 0
+            request.session['original'] = ''
+            request.session['translation'] = ''
+            return redirect('words:congratulations')
+
+    def get(self, request, *args, **kwargs):
+        request.session['current_word_index'] = 0
+        request.session['error_words'] = []
+        messages.success(request, 'Go Go Go')
+        return self._process_word(request, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        user_answer = self.request.POST.get('user_answer', '')
+        get_original = request.session.get('original')
+        get_translation = request.session.get('translation')
+
+        request.session['current_word_index'] += 1
+
+        if user_answer == get_original:
+            messages.success(request, 'Great! You are right!')
+        else:
+            request.session.get('error_words', []).append((get_original, get_translation))
+            messages.error(request, f'Oops.. Error. {get_translation} = {get_original}')
+
+        return self._process_word(request, **kwargs)
+
+
+def show_error_words(request):
+    context = {'title': 'Error Words', 'error_words': request.session['error_words']}
+    return render(request, 'words/error_words.html', context)
+
+
+def reset(request):
+    request.session['current_word_index'] = None
+    request.session['original'] = None
+    request.session['translation'] = None
+
+    return render(request, 'words/finished.html', {'title': 'Congratulations'})
