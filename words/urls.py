5bfc951c83ec5d3ea18d80acd4c072164e72ccdd
Stanislav <nstanislass@gmail.com>
2023-10-04 19:02:04 +0300

    add paths repeat-words congratulations error-words

--- words/urls.py
+++ words/urls.py
@@ -2,7 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from words.views import (AddDictionaryView, AddPairWordView, ImportWordsView,
-                         ShowAllDictionaryUserView, ShowDictionaryView)
+                         RepeatWordsView, ShowAllDictionaryUserView,
+                         ShowDictionaryView, reset, show_error_words)
 
 app_name = 'words'
 
@@ -10,6 +11,12 @@ urlpatterns = [
     path('add-new-dictionary/', login_required(AddDictionaryView.as_view()), name='add_dictionary'),
     path('add-new-words/', login_required(AddPairWordView.as_view()), name='add_new_words'),
     path('import-new-words/', login_required(ImportWordsView.as_view()), name='import_new_words'),
-    path('show-dictionary/<slug:dict_slug>', ShowDictionaryView.as_view(), name='show_dictionary'),
+
     path('show-dictionaries/', ShowAllDictionaryUserView.as_view(), name='show_dictionaries'),
+    path('show-dictionary/<slug:dict_slug>', ShowDictionaryView.as_view(), name='show_dictionary'),
+
+    path('repeat-words/<slug:dict_slug>', RepeatWordsView.as_view(), name='repeat_words'),
+
+    path('congratulations/', reset, name='congratulations'),
+    path('error-words/', show_error_words, name='error_words'),
 ]
