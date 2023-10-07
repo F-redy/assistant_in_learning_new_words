50f9167742adc3b6598a3f91b45d89db1880fcb2
Stanislav <nstanislass@gmail.com>
2023-10-08 01:14:51 +0300

    add get_delete_and_updated_words

--- words/utils.py
+++ words/utils.py
@@ -35,13 +35,40 @@ def get_existing_originals(db_words) -> set[str]:
     return set(pair.original for pair in db_words)
 
 
-def get_unique_pairs(text: str, existing_originals: set[str], sep, id_dictionary: int, PairWord):
+def get_unique_pairs(text: str, existing_originals: set[str], sep, dictionary, PairWord):
     unique_pairs = []
     for pair in text.split('\n'):
         try:
             original, translation = pair.split(sep)
             if original not in existing_originals:
-                unique_pairs.append(PairWord(original=original, translation=translation, dictionary=id_dictionary))
+                unique_pairs.append(PairWord(original=original, translation=translation, dictionary=dictionary))
         except ValueError:
-            unique_pairs.append(PairWord(original=pair, translation='', dictionary=id_dictionary))
+            unique_pairs.append(PairWord(original=pair, translation='', dictionary=dictionary))
     return unique_pairs
+
+
+def get_delete_and_updated_words(formset, PairWord, dictionary):
+    words_to_delete = []
+    updated_words = []
+
+    for cleaned_data in formset.cleaned_data:
+        delete_word = cleaned_data.get('delete')
+        new_original = cleaned_data.get('original')
+        new_translation = cleaned_data.get('translation')
+        old_original = cleaned_data.get('id').original
+        old_translation = cleaned_data.get('id').translation
+
+        if delete_word:
+            words_to_delete.append(cleaned_data.get('id').id)
+
+        elif delete_word is False and (new_original != old_original or new_translation != old_translation):
+
+            updated_word = PairWord(
+                id=cleaned_data.get('id').id,
+                original=cleaned_data.get('original'),
+                translation=cleaned_data.get('translation'),
+                dictionary=dictionary
+            )
+            updated_words.append(updated_word)
+
+    return words_to_delete, updated_words
